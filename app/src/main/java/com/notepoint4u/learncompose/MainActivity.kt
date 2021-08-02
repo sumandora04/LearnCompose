841b9ecf8320b1777c7954ec20c3b75f2af4a343
Suman Shekhar <sumandora04@gmail.com>
2021-08-02 22:46:33 +0530

    Initial commit

---
+++ app/src/main/java/com/notepoint4u/learncompose/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.notepoint4u.learncompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.notepoint4u.learncompose.ui.theme.LearnComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .background(Color.Green)
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+
+                Row(verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    //.fillMaxWidth(0.5f)
+                    .height(300.dp)
+                    .width(200.dp)
+                    .background(Color.Cyan)) {
+                    Text(text = "Hello")
+                    Spacer(modifier = Modifier.absolutePadding(left = 10.dp))
+                    Text(text = "World")
+                }
+            }
+
+        }
+    }
+}
+
